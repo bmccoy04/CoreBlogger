2cfea198d429dd3f543ced6876fe12c52d688a0a
bmccoy04 <bryanmccoy04@gmail.com>
2018-11-02 21:00:06 -0500

    Added an http call and error handling.

--- src/CoreBlogger.Site/Pages/Index.cshtml.cs
+++ src/CoreBlogger.Site/Pages/Index.cshtml.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CoreBlogger.Site.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private ILogger<IndexModel> _logger;
 
+        public string Blogs { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            
+            try 
+            {
+                var httpClient = new HttpClient();
+                var response = httpClient.GetStringAsync("https://api.github.com/repos/bmccoy04/CoreBlogger/contents/BlogEntries/");
+                
+                Blogs = await response;
+            } 
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error");
+                if(ex != null)
+                    ErrorMessage = ex.Message;
+            }
         }
     }
+
 }
