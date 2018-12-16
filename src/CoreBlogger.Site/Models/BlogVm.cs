7a72d48cbcd727c28941853358b6c0e34467122f
Bryan McCoy <bryanmccoy04@gmail.com>
2018-12-16 11:08:48 -0600

    Blog entry page add content (#4)
    * Trying to fix entries.
    
    * Fixed rendering.
    
    * Fixed simple paging for blog entries.

---
+++ src/CoreBlogger.Site/Models/BlogVm.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CoreBlogger.Site.Models
+{
+    public class BlogVm
+    {
+        public  BlogVm(string content, string previousBlogId, string nextBlogId)
+        {
+            Content = content;
+            PreviousBlogId = previousBlogId;
+            NextBlogId = nextBlogId;
+        }
+        public string Content { get; private set; } 
+        public string PreviousBlogId { get; private set; }
+        public string NextBlogId { get; private set; }
+        public bool HasPreviousBlogId => !String.IsNullOrWhiteSpace(this.PreviousBlogId);
+        public bool HasNextBlogId => !String.IsNullOrWhiteSpace(this.NextBlogId);
+    }
+}
--- src/CoreBlogger.Site/Pages/Blogs/Index.cshtml.cs
+++ src/CoreBlogger.Site/Pages/Blogs/Index.cshtml.cs
@@ -2,20 +2,69 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreBlogger.Core.Queries;
+using CoreBlogger.Site.Models;
+using CoreBlogger.Core.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CoreBlogger.Site.Pages.Blogs
 {
     public class IndexModel : PageModel
     {
+        private ILogger<IndexModel> _logger;
+        private IMediator _mediator;
+
         public string Message { get; set; }
 
-        public void OnGet(string id)
+        public BlogVm BlogVm {get;set;}
+
+        public bool HasBlog { get { return this.BlogVm != null; } }
+
+        public IndexModel(ILogger<IndexModel> logger, IMediator mediator)
         {
-            if(String.IsNullOrEmpty(id))
-                this.Message = "NONe";
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public async Task OnGetAsync(string id)
+        {
+            try 
+            {
+                this.BlogVm = await GetBlogVm(id);
+
+                this.Message = id + this.BlogVm;
+            } 
+            catch (Exception ex)
+            {
+                this.Message = ex.Message;
+            }
+
+            
+        }
+
+        private async Task<BlogVm> GetBlogVm(string id)
+        {
+            var entries = await _mediator.Send(new GetBlogEntriesQuery());
+            var entry = new GitHubBlogEntry();
+        
+            if(String.IsNullOrWhiteSpace(id))
+                entry = entries.LastOrDefault();
             else
-                this.Message = id;
+                entry = entries.FirstOrDefault(x => x.GitHubBlogEntryMetaData.Date.ToString("yyyyMMdd") == id);
+        
+            var index = entries.IndexOf(entry);
+            var previousId = String.Empty;
+            var nextId = string.Empty;
+            
+            if(index > 0)
+                previousId = entries[index - 1].GitHubBlogEntryMetaData.Date.ToString("yyyyMMdd");
+            if(index < entries.Count - 1)
+                nextId = entries[index + 1].GitHubBlogEntryMetaData.Date.ToString("yyyyMMdd");
+
+            return new BlogVm(entry.Content, previousId, nextId);
+
         }
     }
 }
